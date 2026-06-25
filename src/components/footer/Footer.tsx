3cea15aaffc9931c132ad5cd1be726d792743ffb
kaua alves <kauaalves7163@icloud.com>
2026-06-25 10:42:40 -0300

    login e cadastro apenas visual

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -9,13 +9,14 @@ function Footer() {
             <div className="flex justify-center bg-indigo-900 text-white">
                 <div className="container flex flex-col items-center py-4">
                     <p className='text-xl font-bold'>
-                        Blog Pessoal Generation | Copyright: {data}
+                        Meu Blog Pessoal  | Copyright: {data}
                     </p>
-                    <p className='text-lg'>Acesse nossas redes sociais</p>
+                    <p className='text-lg'>Acesse nossa rede social</p>
                     <div className='flex gap-2'>
-                        <LinkedinLogoIcon size={48} weight='bold' />
-                        <InstagramLogoIcon size={48} weight='bold' />
-                        <FacebookLogoIcon size={48} weight='bold' />
+                        <a href="https://www.linkedin.com/in/kauaalvesti" target="_blank">
+                            <LinkedinLogoIcon size={48} weight='bold' />
+                        </a>
+                       
                     </div>
                 </div>
             </div>
