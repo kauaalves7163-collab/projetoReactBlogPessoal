1e837cd9b14da4de2e1b215adab5740c3916d126
kaua alves <kauaalves7163@icloud.com>
2026-06-30 16:36:49 -0300

    cookbook ate o 22

---
+++ src/services/Service.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: 'https://blogpessoal-71so.onrender.com'
+})
+
+export const cadastrarUsuario = async (url: string, dados: Object, setDados: Function) => {
+    const resposta = await api.post(url, dados)
+    setDados(resposta.data)
+}
+
+export const login = async (url: string, dados: Object, setDados: Function) => {
+    const resposta = await api.post(url, dados)
+    setDados(resposta.data)
+}
+
+export const buscar = async (url: string, setDados: Function, header: Object) => {
+    const resposta = await api.get(url, header)
+    setDados(resposta.data)
+}
