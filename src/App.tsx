f711d00c5a6f5fd47e561d86e8cca7f9e9d33001
kaua alves <kauaalves7163@icloud.com>
2026-07-02 11:48:45 -0300

    Deletar Postagem

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,9 @@ import Cadastro from './pages/cadastro/Cadastro'
 import Home from './pages/home/Home'
 import Login from './pages/login/Login'
 import DeletarTema from './components/tema/deletartema/DeletarTema'
+import ListaPostagens from './components/postagem/listapostagens/ListaPostagens'
+import FormPostagem from './components/postagem/formpostagem/FormPostagem'
+import DeletarPostagem from './components/postagem/deletarpostagem/DeletarPostagem'
 
 function App() {
 	return (
@@ -19,11 +22,15 @@ function App() {
 						<Routes>
 							<Route path="/" element={<Login />} />
 							<Route path="/home" element={<Home />} />
-							<Route path="/cadastro"	element={<Cadastro />}/>
+							<Route path="/cadastro" element={<Cadastro />} />
 							<Route path="/temas" element={<ListaTemas />} />
 							<Route path="/cadastrartema" element={<FormTema />} />
 							<Route path="/editartema/:id" element={<FormTema />} />
-              <Route path="/deletartema/:id" element={<DeletarTema />} />
+							<Route path="/deletartema/:id" element={<DeletarTema />} />
+							<Route path="/postagens" element={<ListaPostagens />} />
+							<Route path="/cadastrarpostagem" element={<FormPostagem />} />
+							<Route path="/editarpostagem/:id" element={<FormPostagem />} />
+							<Route path="/deletarpostagem/:id" element={<DeletarPostagem />} />
 						</Routes>
 					</div>
 					<Footer />
--- src/models/Postagem.ts
+++ src/models/Postagem.ts
@@ -1,7 +1,11 @@
-import type Postagem from "./Postagem";
+import type Tema from './Tema';
+import type Usuario from './Usuario';
 
-export default interface Tema {
-    id: number;
-    descricao: string;
-    postagem?: Postagem[] | null;
+export default interface Postagem {
+  id: number;
+  titulo: string;
+  texto: string;
+  data: string;
+  tema: Tema | null;
+  usuario: Usuario | null;
 }
